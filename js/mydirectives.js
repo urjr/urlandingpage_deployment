bf44dff98854a62d17fc34206f864024e9bd9041
urjr <urjr8891@gmail.com>
2015-04-03 00:48:42 -0700

    v0.4.6 (added second directive to navbar)

--- js/mydirectives.js
+++ js/mydirectives.js
@@ -10,6 +10,7 @@ siteDirectives.directive('urDarken', function(){
 			// check if menu background change was triggered
 			var changeTriggered = false;
 
+			// checks for css tablet breakpoint, removes newly added dark background when larger than 768px
 			function checkWindowWidth(){
 				$(window).resize(function(){
 					if(changeTriggered){
@@ -23,6 +24,7 @@ siteDirectives.directive('urDarken', function(){
 				});
 			};
 
+			//darkens navbar background when event is triggered
 			scope.$watch(attr.urDarken, function(shouldDarken){
 				if(shouldDarken){
 					element.css(darkBg);
@@ -34,5 +36,22 @@ siteDirectives.directive('urDarken', function(){
 				}
 			});
 		},
+	}
+});
+
+siteDirectives.directive('urFlip', function(){
+	return{
+		restrict: 'A',
+		link: function(scope, element){
+			var arrowDown = 'fa-caret-square-o-down';
+			var arrowUp = 'fa-caret-square-o-up';
+
+			element.addClass(arrowDown);
+
+			element.click(function(){
+				element.toggleClass(arrowDown);
+				element.toggleClass(arrowUp);
+			});
+		}
 	}
 });
